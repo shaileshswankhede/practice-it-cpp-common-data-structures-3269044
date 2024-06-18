f9085367e73b539aab3431e48ccb87b71343821a
Shailesh Wankhede <shaileshswankhede@gmail.com>
2024-06-18 18:03:20 +0000

    Completed SJF implementation

--- src/OS_concept/Scheduling/SJF.cpp
+++ src/OS_concept/Scheduling/SJF.cpp
@@ -11,19 +11,35 @@ bool cmpArrival(const Process &p1, const Process &p2)
 void runSJFS(Process p[], int num)
 {
   sort(p, p + num, cmpArrival);
+
   // wait queue to maintain process with min service time at top
   priority_queue<Process> pq;
+  uint32_t currTime = p[0].arrival;
+  uint32_t totalTat = 0;
 
-  uint32_t idx = 0;
-  uint64_t currTime = 0;
-  while (pq.size() || idx < num)
+  // push first process in wait queue
+  pq.push(p[0]);
+  p[0].arrival = -1; // set process arrival to -1 so that it doesn't enter queue again
+
+  while (!pq.empty())
   {
-    // while task's arrival time is less than current time, add it to pq
-    while (idx < num and p[idx].arrival <= max(p[0].arrival, (uint32_t)currTime))
-      pq.push(p[idx++]);
+    printf("Running Process P%d\n", pq.top().pid);
 
-    // if queue is empty start with next process
-    if (pq.size() == 0)
-      pq.push(p[idx++]);
+    // increase currTime with service time of currently executed process
+    // and remove executed process from wait queue
+    currTime += pq.top().service;
+    totalTat += (currTime - pq.top().arrival);
+    pq.pop();
+
+    // add processes to queue while their arrival time is less than current time based on their service time
+    for (int idx = 0; idx < num; idx++)
+    {
+      if ((p[idx].arrival <= currTime) && (p[idx].arrival != -1))
+      {
+        pq.push(p[idx]);
+        p[idx].arrival = -1;
+      }
+    }
   }
+  printf("avg TAT for %d processes = %.2f\n", num, totalTat * 1.0f / num);
 }
