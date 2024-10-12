79063c6f8730df51bba1f2e14f37b103ef41b2c7
Shailesh Wankhede <shaileshswankhede@gmail.com>
2024-10-12 11:47:11 +0000

    structure alignment concept code

--- src/test_main.cpp
+++ src/test_main.cpp
@@ -1,5 +1,20 @@
 #include <stdio.h>
 #include <string.h>
+#include <iostream>
+
+typedef struct
+{
+  char a;  // 1 byte
+  char *c; // pointer (size depends on the machine: 4 bytes on 32-bit, 8 bytes on 64-bit)
+  int b;   // 4 byte
+} myStruct;
+
+struct Example2
+{
+  char a;   // 1 byte
+  int b;    // 4 bytes
+  double c; // 8 bytes
+};
 
 int *retArray(void)
 {
@@ -10,10 +25,15 @@ int *retArray(void)
 
 int main()
 {
+  /*
   int *ptr = retArray();
   for (int i = 0; i < 100; i++)
   {
     printf("0x%x ", ptr[i]);
   }
+  */
+  printf("sizeof myStruct %d\n", sizeof(myStruct));
+  printf("sizeof Example2 %d\n", sizeof(Example2));
+
   return 0;
 }
